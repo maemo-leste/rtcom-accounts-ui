946c15224a8b1914159e19f75a51d960c44dbb5b
Merlijn Wajer <merlijn@wizzup.org>
2025-01-04 19:17:10 +0100

    widgets: stop using volatile

--- widgets/rtcom-widget.c
+++ widgets/rtcom-widget.c
@@ -47,9 +47,9 @@ enum
 GType
 rtcom_widget_get_type(void)
 {
-  static volatile gsize g_define_type_id__volatile = 0;
+  static gsize g_define_type_id = 0;
 
-  if (g_once_init_enter(&g_define_type_id__volatile))
+  if (g_once_init_enter(&g_define_type_id))
   {
     static const GTypeInfo info =
     {
@@ -71,10 +71,10 @@ rtcom_widget_get_type(void)
                              &info,
                              0);
 
-    g_once_init_leave(&g_define_type_id__volatile, g_define_type_id);
+    g_once_init_leave(&g_define_type_id, g_define_type_id);
   }
 
-  return g_define_type_id__volatile;
+  return g_define_type_id;
 }
 
 static void
